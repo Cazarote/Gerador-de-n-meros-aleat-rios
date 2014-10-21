3b355ed01cba39ddb14517f900080b5c8bedc949
tisouro <tisouro@gspd-shion.lab.gspd>
2014-10-21 21:45:13 -0200

    alteraçoes

--- random.c
+++ random.c
@@ -1,7 +1,7 @@
 /* Autor: Lucas Afonso Cazarote
  * Data: 25 de Setembro de 2014
  * 
- * Programa que gera uma sequência de números aleatórios dentro de um intervalo definido pelo usuário.
+ * que gera uma sequência de números aleatórios dentro de um intervalo definido pelo usuário.
  */
 
 
@@ -9,10 +9,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define CYAN "\033[46;30;1m"
+#define NORMAL "\033[0m"
+#define BRANCO "\033[47;30;1m"
+
 
 int main()
 {
-	int i, limiteInferior, limiteSuperior, intervalo, offset, tmp, elementos;
+	int linha, coluna, limiteInferior, limiteSuperior, intervalo, offset, tmp, elementos, colunas, linhas;
 
 /* variável offset é o salto que ele vai dar de 0 até o limite inferior	
 Exemplo: limite inferior 200, limite superior 320, 320 - 200 = 120 elementos
@@ -30,17 +34,38 @@ Exemplo: limite inferior 200, limite superior 320, 320 - 200 = 120 elementos
 	printf("Digite o numero de elementos a serem gerados: ");
 	scanf("%d",&elementos);
 
+	printf("Digite a quantidade de colunas desejadas: ");
+	scanf("%d",&colunas);
+
 	intervalo = limiteSuperior - limiteInferior;
-	offset = limiteInferior;	
+	offset = limiteInferior;
 	
+	linhas = elementos/colunas;
+
 	printf("Os numeros gerados são:\n");
 
-	for(i=0;i<elementos;i++)
-	{		
-		tmp = rand()%(intervalo + 1) + offset;
-		printf("%d \n",tmp);
-	}
+	for(linha=0;linha<linhas;linha++)
+	{	
+	
+		if(linha%2 == 0)
+		{
+			printf("%s", CYAN);
+		}
 
+		else
+		{
+			printf("%s",BRANCO);
+		}
+
+		for(coluna=0;coluna<colunas;coluna++)
+		{
+
+			tmp = rand()%(intervalo + 1) + offset;
+			printf(" %8d ", tmp);
+		}
+
+		printf("%s\n", NORMAL);
+	}
 	
 	return 0;
 
